f8287abac00db11367c2f2cb64752af9ae5ad7f5
Dave Arter <davea@mysociety.org>
2016-08-24 15:20:57 +0100

    fixup! Allow response templates to be assigned to categories

--- db/schema.sql
+++ db/schema.sql
@@ -480,5 +480,5 @@ create table response_templates (
 CREATE TABLE contact_response_templates (
     id serial NOT NULL PRIMARY KEY,
     contact_id int REFERENCES contacts(id) NOT NULL,
-    response_template_id int REFERENCES response_templates(id) NOT NULL,
+    response_template_id int REFERENCES response_templates(id) NOT NULL
 );
